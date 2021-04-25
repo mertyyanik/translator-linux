79597b4fdd69730305bf07c3519aa84dab85288d
Mert Yanık <mert19971997@gmail.com>
2021-04-25 09:44:09 +0300

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,28 @@
+from googletrans import Translator
+import subprocess
+import keyboard
+import re
+import sys
+
+def run(text):
+	translator = Translator()
+	text = re.sub(r'[\t\r\n]+', ' ', text)
+	result = translator.translate(text, src = sys.argv[1], dest = sys.argv[2])
+	subprocess.run(['gxmessage', f'{result.text}'])
+
+def get_copied_text():
+	text = subprocess.check_output('xsel -b', shell=True)
+	if text == b'':
+		return None
+	return text
+
+def main():
+	while True:
+		keyboard.wait('q')
+		text = get_copied_text()
+		if text is None:
+			continue
+		run(text.decode('utf-8'))
+
+if __name__ == '__main__':
+	main()
